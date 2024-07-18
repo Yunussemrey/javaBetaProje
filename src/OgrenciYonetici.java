36030c66671e35544e7250b51209fc89e1dcd690
Yunus Emre Akgül <yunussemrey1@gmail.com>
2024-07-18 14:37:04 +0300

    Projeme yeni bi öğrenci ekledim

--- src/OgrenciYonetici.java
+++ src/OgrenciYonetici.java
@@ -1,9 +1,11 @@
 public class OgrenciYonetici {
     Ogrenci ogrenci = new Ogrenci("Yunus Emre","Akgül",23,true,true,"Giresun");
     Ogrenci ogrenci2 = new Ogrenci("csdcdscsc","scsac",22,true,true,"fdvfdvfd");
+    Ogrenci ogrenci3 = new Ogrenci("Kaan","sasd",25,true,true,"İstanbul");
     public void ogrenciYazdir() {
         System.out.println("Öğrenci adı: "+ogrenci.getAd());
         System.out.println("Öğrenci soyadı: "+ogrenci.getSoyad());
+        System.out.println("yeni öğrenci "+ ogrenci3.getAd());
     }
     public void ogrenciEkle() {
 
